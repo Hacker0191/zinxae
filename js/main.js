0fcff74d49c5e366dd63e4a7e328735f6af0ae16
Hacker0191 <themayankgamerz@gmail.com>
2024-01-19 07:54:16 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,320 @@
+// Preloader Starts
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const charactersArray = characters.split("");
+
+const fontSize = 16;
+const speed = 3;
+
+const canvas = document.getElementById("matrix");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const columns = Math.floor(canvas.width / fontSize);
+const rows = Math.floor(canvas.height / fontSize);
+
+const matrix = [];
+for (let i = 0; i < columns; i++) {
+  matrix[i] = 1;
+}
+
+function drawMatrix() {
+  context.fillStyle = "rgba(0, 0, 0, 0.05)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "#00FF00";
+  context.font = fontSize + "px monospace";
+
+  for (let i = 0; i < matrix.length; i++) {
+    const text = charactersArray[Math.floor(Math.random() * charactersArray.length)];
+    context.fillText(text, i * fontSize, matrix[i] * fontSize);
+
+    if (matrix[i] * fontSize > canvas.height && Math.random() > 0.975) {
+      matrix[i] = 0;
+    }
+
+    matrix[i]++;
+  }
+}
+
+function animateMatrix() {
+  drawMatrix();
+  requestAnimationFrame(animateMatrix);
+}
+
+animateMatrix();
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var preloader = document.getElementById("matrix");
+      preloader.style.display = "none";
+    }, 3000);
+  });
+//  Preloader Ends 
+
+// Text Preloader Starts
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var preloader = document.getElementById("preloader");
+      preloader.style.opacity = "1";
+  
+      var loadingText = document.getElementById("loading-text");
+      loadingText.style.opacity = "1";
+      loadingText.style.transform = "translateY(0)";
+  
+      setTimeout(function() {
+        preloader.style.opacity = "0";
+      }, 1500);
+    }, 3000);
+  });
+//  Text Preloader Ends
+  
+
+// TypeScript
+var typing = new Typed(".text", {
+    strings: ["", "websites.", "graphics.", "games."],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true
+  });
+  
+  // GSAP
+  gsap.registerPlugin(ScrollTrigger);
+  const introsplitTypes = document.querySelectorAll(".left-part h1");
+  introsplitTypes.forEach((char, i) => {
+    const i_text = new SplitType(char);
+    gsap.to(i_text.chars, {
+      y: 0,
+      stagger: 0.08, // text splitting transition
+      duration: 0.3 // full text duration
+    });
+  });
+
+
+  
+
+
+
+  gsap.registerPlugin(ScrollTrigger);
+
+const textElements = gsap.utils.toArray('.text-main-home');
+
+textElements.forEach(text => {
+  gsap.to(text, {
+    backgroundSize: '100%',
+    ease: 'none',
+    scrollTrigger: {
+      trigger: text,
+      start: 'center 80%',
+      end: 'center 20%',
+      scrub: true,
+    },
+  });
+});
+
+
+
+const circleEls = document.querySelectorAll('.js-circle')
+const circleTl = gsap.timeline({repeat: -1, yoyo: true});
+
+let circleSize = 1;
+circleEls.forEach(circle => {
+  gsap.set(circle, {scale: circleSize});
+  circleSize+=.5
+})
+
+circleTl.from(circleEls, {scale: 1, stagger: .175, duration: .6, ease: Power3.inOut})
+
+
+
+
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+
+
+let start = new Date().getTime();
+
+const originPosition = { x: 0, y: 0 };
+
+const last = {
+  starTimestamp: start,
+  starPosition: originPosition,
+  mousePosition: originPosition
+}
+
+const config = {
+  starAnimationDuration: 1500,
+  minimumTimeBetweenStars: 250,
+  minimumDistanceBetweenStars: 75,
+  glowDuration: 75,
+  maximumGlowPointSpacing: 10,
+  colors: ["249 146 253", "252 254 255"],
+  sizes: ["1.4rem", "1rem", "0.6rem"],
+  animations: ["fall-1", "fall-2", "fall-3"]
+}
+
+let count = 0;
+  
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+      selectRandom = items => items[rand(0, items.length - 1)];
+
+const withUnit = (value, unit) => `${value}${unit}`,
+      px = value => withUnit(value, "px"),
+      ms = value => withUnit(value, "ms");
+
+const calcDistance = (a, b) => {
+  const diffX = b.x - a.x,
+        diffY = b.y - a.y;
+  
+  return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+}
+
+const calcElapsedTime = (start, end) => end - start;
+
+const appendElement = element => document.body.appendChild(element),
+      removeElement = (element, delay) => setTimeout(() => document.body.removeChild(element), delay);
+
+const createStar = position => {
+  const star = document.createElement("span"),
+        color = selectRandom(config.colors);
+  
+  star.className = "star fa-solid fa-sparkle";
+  
+  star.style.left = px(position.x);
+  star.style.top = px(position.y);
+  star.style.fontSize = selectRandom(config.sizes);
+  star.style.color = `rgb(${color})`;
+  star.style.textShadow = `0px 0px 1.5rem rgb(${color} / 0.5)`;
+  star.style.animationName = config.animations[count++ % 3];
+  star.style.starAnimationDuration = ms(config.starAnimationDuration);
+  
+  appendElement(star);
+
+  removeElement(star, config.starAnimationDuration);
+}
+
+const createGlowPoint = position => {
+  const glow = document.createElement("div");
+  
+  glow.className = "glow-point";
+  
+  glow.style.left = px(position.x);
+  glow.style.top = px(position.y);
+  
+  appendElement(glow)
+  
+  removeElement(glow, config.glowDuration);
+}
+
+const determinePointQuantity = distance => Math.max(
+  Math.floor(distance / config.maximumGlowPointSpacing),
+  1
+);
+
+/* --  
+
+The following is an explanation for the "createGlow" function below:
+
+I didn't cover this in my video, but I ran into an issue where moving the mouse really quickly caused gaps in the glow effect. Kind of like this:
+
+*   *       *       *    *      *    🖱️
+
+instead of:
+
+*************************************🖱️
+
+To solve this I sort of "backfilled" some additional glow points by evenly spacing them in between the current point and the last one. I found this approach to be more visually pleasing than one glow point spanning the whole gap.
+
+The "quantity" of points is based on the config property "maximumGlowPointSpacing".
+
+My best explanation for why this is happening is due to the mousemove event only firing every so often. I also don't think this fix was totally necessary, but it annoyed me that it was happening so I took on the challenge of trying to fix it.
+
+-- */
+const createGlow = (last, current) => {
+  const distance = calcDistance(last, current),
+        quantity = determinePointQuantity(distance);
+  
+  const dx = (current.x - last.x) / quantity,
+        dy = (current.y - last.y) / quantity;
+  
+  Array.from(Array(quantity)).forEach((_, index) => { 
+    const x = last.x + dx * index, 
+          y = last.y + dy * index;
+    
+    createGlowPoint({ x, y });
+  });
+}
+
+const updateLastStar = position => {
+  last.starTimestamp = new Date().getTime();
+
+  last.starPosition = position;
+}
+
+const updateLastMousePosition = position => last.mousePosition = position;
+
+const adjustLastMousePosition = position => {
+  if(last.mousePosition.x === 0 && last.mousePosition.y === 0) {
+    last.mousePosition = position;
+  }
+};
+
+const handleOnMove = e => {
+  const mousePosition = { x: e.clientX, y: e.clientY }
+  
+  adjustLastMousePosition(mousePosition);
+  
+  const now = new Date().getTime(),
+        hasMovedFarEnough = calcDistance(last.starPosition, mousePosition) >= config.minimumDistanceBetweenStars,
+        hasBeenLongEnough = calcElapsedTime(last.starTimestamp, now) > config.minimumTimeBetweenStars;
+  
+  if(hasMovedFarEnough || hasBeenLongEnough) {
+    createStar(mousePosition);
+    
+    updateLastStar(mousePosition);
+  }
+  
+  createGlow(last.mousePosition, mousePosition);
+  
+  updateLastMousePosition(mousePosition);
+}
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+document.body.onmouseleave = () => updateLastMousePosition(originPosition);
+
+
+
+
+const buttons = document.querySelectorAll('.faq-toggle');
+
+
+buttons.forEach(button =>{
+  button.addEventListener('click', () =>{
+    button.parentNode.classList.toggle('active');
+  })
+})
+ 
